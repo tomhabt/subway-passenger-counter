6c69f980330e0f021be88c5b59adbdd6dca7172f
Thomas Habtemariam <84083304+tomhabt@users.noreply.github.com>
2022-01-25 22:19:00 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var countEl = document.getElementById("count-el");
+var incrementEl = document.getElementById("btn-inc");
+var saveEl = document.getElementById("btn-save");
+
+console.log(countEl);
+
+function count () {
+    onclick(incrementEl)
+}
